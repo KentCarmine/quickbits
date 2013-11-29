b3357bcd608ce626cda6d17786441da8331e0720
Apprentice <apprentice@dbc-chicago031.local>
2013-11-29 11:45:17 -0600

    Initial commit to setup Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quickbits::Application.config.secret_token = '531ae4f37652c940e816432610aab8d567ab616e1494edd5dc7430f640b23c34322d47496b2dcf9aa5b5c08a351097e83d7c38cc2dbcc92f470018043e2292b3'
