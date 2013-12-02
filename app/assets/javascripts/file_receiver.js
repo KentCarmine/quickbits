7be1cdfdd818bd097d3a867d68c2f367c40249f5
Kotentie <jakekoten@gmail.com>
2013-12-02 14:26:31 -0600

    Removed extraneous comments and methods from file_sender and file_receiver

--- app/assets/javascripts/file_receiver.js
+++ app/assets/javascripts/file_receiver.js
@@ -1,14 +1,11 @@
 // RECEIVER SIDE
-
 function Receiver(){
   var thisReceiver = this;
-  thisReceiver.peer = new Peer({ key: "ry9t770xq4hx5hfr" });
+  thisReceiver.peer = new Peer({ key: "2pmakgfy6gw7mn29" });
 }
 
 Receiver.prototype.establishConnection = function(senderPeerId){
   var thisReceiver = this;
-  // console.log("establishConnection called");
-
   thisReceiver.connection = thisReceiver.peer.connect(senderPeerId);
 }
 
@@ -20,15 +17,11 @@ Receiver.prototype.handleConnection = function(){
 }
 
 Receiver.prototype.getData = function(){
-  // console.log("getData called");
   var fileArray = [];
   var thisReceiver = this;
   thisReceiver.connection.on("data", function(data){
-    // console.log("data received in getData");
 
     if(data.isFileMetaData){
-      // console.log("GOT: " + data); //tester code
-      // console.log("GOT: " + data.fileSize);
       console.log("got fileMetaData, should not be here!");
     }
     else if(data.isFile){
@@ -40,25 +33,12 @@ Receiver.prototype.getData = function(){
       //ALTER OR REMOVE TIMEOUT LATER!
       //Maybe by checking if file is complete?
       if(data.isLast == 1){
-        // console.log("in data.islast");
         setTimeout(function(){
         var fileConstruct = new Blob(fileArray);
         saveAs(fileConstruct, data.fileName);}, 500);
       }
-
-      // debugger;
-      // console.log(file);
-      // console.log(data.arrayBufferFileData);
-      // saveAs(file, data.fileName);
     }
-
-    // thisReceiver.sendData("I got it!"); //tester code
   });
-}
-
-Receiver.prototype.sendData = function(dataToSend){
-  var thisReceiver = this;
-  thisReceiver.connection.send(dataToSend); //tester code
 }
 
 function byteConverter(bytes){
@@ -81,14 +61,9 @@ function byteConverter(bytes){
 }
 
 $(function(){
-  // debugger;
   var splitUrl = window.location.href.split("/");
   var senderPeerId = splitUrl[splitUrl.length-1];
-  // console.log(senderPeerId);
   var receiver = new Receiver();
-  // var senderPeerId = $("p").text();
-  // console.log(senderPeerId);
-  // debugger;
 
   receiver.establishConnection(senderPeerId);
   receiver.handleConnection();
