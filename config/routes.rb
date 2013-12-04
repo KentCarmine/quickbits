c237f39c449d88669c0c4d2bd6ed96e9191138e1
Kotentie <jakekoten@gmail.com>
2013-12-04 11:21:56 -0600

    Removed old testing controller, views, and routes

--- config/routes.rb
+++ config/routes.rb
@@ -56,11 +56,6 @@ Quickbits::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
 
-  get '/send' => "peer_connection#sendTestData"
-  get '/receive/:senderPeerId' => "peer_connection#receiveTestData"
-
-  mount JasmineRails::Engine => "/specs" if defined?(JasmineRails)
-
   get '/:connection_id', :to => 'receivers#show'
   resources :initiators
   root to: "initiators#index"
