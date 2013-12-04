76652221cfef13defbed309ac97716862e8a5bb7
Kotentie <jakekoten@gmail.com>
2013-12-04 13:40:07 -0600

    Fixed spelling and grammar mistakes

--- app/assets/javascripts/file_receiver.js
+++ app/assets/javascripts/file_receiver.js
@@ -64,7 +64,7 @@ Receiver.prototype.getData = function(){
   var errorElement = $("#error_message");
   var upload_button = document.querySelector('.button_upload');
   thisReceiver.connection.on("data", function(data){
-  alert_browser.innerHTML = "<h3>Your file is being tranferred</h3>";
+  alert_browser.innerHTML = "<h3>Your file is being transferred</h3>";
   alert_browser.style.display = "inline";
 
     if(data.isFileMetaData){
