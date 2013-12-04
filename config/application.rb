d6ce14904bed84a01d4ca10b660a51253e750666
Chirag Tailor <chiragtailor@Chirags-MacBook-Pro.local>
2013-12-04 14:24:17 -0600

    Precompile assets locally and added fallback to precompile assets in config file

--- config/application.rb
+++ config/application.rb
@@ -68,5 +68,7 @@ module Quickbits
     config.force_ssl = false
 
     config.assets.initialize_on_precompile = false
+
+    config.assets.precompile += ['file_sender.js', 'file_receiver.js']
   end
 end
