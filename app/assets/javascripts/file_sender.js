2c99c15d365c21c977f72e717d5ae0c801cdec71
Clark Kampfe <clark.kampfe@gmail.com>
2013-12-04 18:13:59 -0600

    fix merge conflict left in code

--- app/assets/javascripts/file_sender.js
+++ app/assets/javascripts/file_sender.js
@@ -109,16 +109,10 @@ Sender.prototype.sendFile = function(){
   var fileReader = new FileReader();
   fileReader.readAsArrayBuffer(thisSender.file);
 
-<<<<<<< HEAD
-
   window.onbeforeunload = function() {
     return "If you close the window the file will not finish transfer.";
   };
 
-
-
-=======
->>>>>>> master
   fileReader.onload = function(){
     var fileData = fileReader.result;
     var blob = [];
